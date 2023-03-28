8304e6e803a0c2c5cbfc629b64158ecc857cb99a
LpgLevin <lpglevin@gmail.com>
2023-03-28 13:10:02 +0100

    GET /api/articles/:article_id

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 const express = require('express');
-const { getTopics } = require('./controllers/topics.controller')
-const { invalidPathwayError} = require('./controllers/errors.controller');
+const { getTopics, getArticleById } = require('./controllers/topics.controller')
+const { invalidPathwayError, customError, psqlError400, serverError500 } = require('./controllers/errors.controller');
+
+
 
 const app = express();
 
 app.get('/api/topics', getTopics);
 
+app.get('/api/articles/:article_id', getArticleById);
+
 app.all('/*', invalidPathwayError);
+
+app.use(customError);
+
+app.use(psqlError400);
+
+app.use(serverError500);
 
 module.exports = app;
--- controllers/topics.controller.js
+++ controllers/topics.controller.js
@@ -1,8 +1,26 @@
-const { selectTopics } = require('../models/topics.model');
+const { selectTopics, selectArticleById } = require('../models/topics.model');
 
 exports.getTopics = (request, response, next) =>{
 
     selectTopics()
     .then((arrayOfTopics) => { response.status(200).send({ topics : arrayOfTopics })})
     .catch(next);
+};
+
+exports.getArticleById = (request, response, next) => {
+
+    const { article_id } = request.params;
+
+    selectArticleById(article_id)
+    .then((articleObject) => {
+        response.status(200)
+        .send({ articleObject })
+       
+    })
+
+    .catch((err) => {
+
+        next(err);
+
+    });
 };
